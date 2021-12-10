a83ab788516949aac54992906c31f8fbcab35389
Jess Frazelle <github@jessfraz.com>
2021-12-09 20:24:34 -0800

    update
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- cmd/meta/instance/instance.go
+++ cmd/meta/instance/instance.go
@@ -52,21 +52,11 @@ func NewCmdInstance(cli *cli.CLI) *cobra.Command {
 				`),
 
 		RunE: func(cmd *cobra.Command, args []string) error {
-			/*			kittycadClient, err := cli.KittyCADClient()
-						if err != nil {
-							return err
-						}
-
-						instance, err := kittycadClient.MetaDebugInstance(cli.Context)
-						if err != nil {
-							return fmt.Errorf("failed to get auth instance: %w", err)
-						}
-
-						fmt.Printf("%#v\n", instance)*/
 			return instanceRun(opts)
 		},
 	}
 
+	// TODO: Actually get the JSON flags to work.
 	cmdutil.AddJSONFlags(cmd, &opts.Exporter, defaultFields)
 
 	return cmd
