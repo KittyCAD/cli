48fcd002f1bae0f495bece109fd8356725792469
Kurt Hutten IrevDev <k.hutten@protonmail.ch>
2022-11-02 06:46:52 +1100

    create tar.gz and add sha to tap .rb file

---
+++ homebrew-tap-template.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class KittyCAD < Formula
+    desc "KittyCAD CLI for interacting with our API"
+    homepage "https://kittycad.io/docs/cli/manual"
+    url "replace-with-url"
+    sha256 "replace-with-sha"
+    version "replace-with-semver"
+  
+    def install
+      bin.install "kittycad"
+    end
+  end
