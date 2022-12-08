692fade2ff6e8aa28c89aa2141d934babfe1b865
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2022-12-08 14:16:16 +1100

    move script into script

--- homebrew.sh
+++ homebrew.sh
@@ -16,10 +16,10 @@ homebrew_names=(
   "aarch64_linux"
 )
 
-version=v$(toml get Cargo.toml package.version | jq -r .)
 to_zip_files=""
+version=v$(toml get Cargo.toml package.version | jq -r .)
+sed -i "s#replace-semver#$version#g" "./homebrew/kittycad.rb"
 
-# iterate over the input names
 for i in "${!input_names[@]}"; do
   input_name="${input_names[$i]}"
   homebrew_name="${homebrew_names[$i]}"
@@ -27,26 +27,20 @@ for i in "${!input_names[@]}"; do
   mkdir "./homebrew/$homebrew_name"
   curl -L "https://dl.kittycad.io/releases/cli/$version/kittycad-$input_name" -o "./homebrew/$homebrew_name/kittycad"
 
-  # create variable with the sha256sum of the file without the filename
   sha256=$(sha256sum "./homebrew/$homebrew_name/kittycad")
   hash=$(printf '%s\n' "$sha256" | cut -d' ' -f1)
-  sed -i '' "s#replace-$homebrew_name-sha#$hash#g" "./homebrew/kittycad.rb"
+  sed -i "s#replace-$homebrew_name-sha#$hash#g" "./homebrew/kittycad.rb"
 
-  # concat the strings homebrew/x86_64_darwin/kittycad into a variable to_zip_files
   to_zip_files="$to_zip_files $homebrew_name/kittycad"
-
 done
-
 
 (cd ./homebrew && tar -czvf kittycad-cli.tar.gz $to_zip_files)
 
 sha256=$(sha256sum "./homebrew/kittycad-cli.tar.gz")
 hash=$(printf '%s\n' "$sha256" | cut -d' ' -f1)
-sed -i '' "s#replace-tarball-sha#$hash#g" "./homebrew/kittycad.rb"
+sed -i "s#replace-tarball-sha#$hash#g" "./homebrew/kittycad.rb"
 
-echo $version
-sed -i '' "s#replace-semver#$version#g" "./homebrew/kittycad.rb"
-
+# clean up
 for homebrew_name in "${homebrew_names[@]}"; do
   rm -rf "./homebrew/$homebrew_name"
 done
