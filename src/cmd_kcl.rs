bc0b106a33577b86ed3f460d2887bc18fe8be4a8
Jess Frazelle <github@jessfraz.com>
2023-10-13 11:36:26 -0700

    add mass etc to kcl
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/cmd_kcl.rs
+++ src/cmd_kcl.rs
@@ -16,6 +16,11 @@ enum SubCommand {
     Export(CmdKclExport),
     Snapshot(CmdKclSnapshot),
     View(CmdKclView),
+    Volume(CmdKclVolume),
+    Mass(CmdKclMass),
+    CenterOfMass(CmdKclCenterOfMass),
+    Density(CmdKclDensity),
+    SurfaceArea(CmdKclSurfaceArea),
 }
 
 #[async_trait::async_trait(?Send)]
@@ -25,6 +30,11 @@ impl crate::cmd::Command for CmdKcl {
             SubCommand::Export(cmd) => cmd.run(ctx).await,
             SubCommand::Snapshot(cmd) => cmd.run(ctx).await,
             SubCommand::View(cmd) => cmd.run(ctx).await,
+            SubCommand::Volume(cmd) => cmd.run(ctx).await,
+            SubCommand::Mass(cmd) => cmd.run(ctx).await,
+            SubCommand::CenterOfMass(cmd) => cmd.run(ctx).await,
+            SubCommand::Density(cmd) => cmd.run(ctx).await,
+            SubCommand::SurfaceArea(cmd) => cmd.run(ctx).await,
         }
     }
 }
@@ -330,4 +340,367 @@ fn get_output_format(
             units: src_unit,
         },
     }
+}
+
+/// Get the volume of an object in a kcl file.
+///
+///     # get the volume of a file
+///     $ kittycad kcl volume --src_unit=m my-file.kcl
+///
+///     # pass a file from stdin
+///     $ cat my-file.kcl | kittycad kcl volume --src_unit=m
+#[derive(Parser, Debug, Clone)]
+#[clap(verbatim_doc_comment)]
+pub struct CmdKclVolume {
+    /// The path to the input file.
+    /// If you pass `-` as the path, the file will be read from stdin.
+    #[clap(name = "input", required = true)]
+    pub input: std::path::PathBuf,
+
+    /// Output format.
+    #[clap(long, short, value_enum)]
+    pub format: Option<crate::types::FormatOutput>,
+
+    /// The source unit to use for the kcl file.
+    #[clap(long, short = 's', value_enum)]
+    pub src_unit: kittycad::types::UnitLength,
+
+    /// Output unit.
+    #[clap(long = "output-unit", short = 'u', value_enum)]
+    pub output_unit: kittycad::types::UnitVolume,
+}
+
+#[async_trait::async_trait(?Send)]
+impl crate::cmd::Command for CmdKclVolume {
+    async fn run(&self, ctx: &mut crate::context::Context) -> Result<()> {
+        // Get the contents of the input file.
+        let input = ctx.read_file(self.input.to_str().unwrap_or(""))?;
+        // Parse the input as a string.
+        let input = std::str::from_utf8(&input)?;
+
+        // Spin up websockets and do the conversion.
+        // This will not return until there are files.
+        let resp = ctx
+            .send_modeling_cmd(
+                "",
+                input,
+                kittycad::types::ModelingCmd::Volume {
+                    entity_ids: vec![], // get whole model
+                    output_unit: self.output_unit.clone(),
+                    source_unit: self.src_unit.clone(),
+                },
+            )
+            .await?;
+
+        if let kittycad::types::OkWebSocketResponseData::Modeling {
+            modeling_response: kittycad::types::OkModelingCmdResponse::Volume { data },
+        } = &resp
+        {
+            // Print the output.
+            let format = ctx.format(&self.format)?;
+            ctx.io.write_output(&format, &data.volume)?;
+        } else {
+            anyhow::bail!("Unexpected response from engine: {:?}", resp);
+        }
+
+        Ok(())
+    }
+}
+
+/// Get the mass of objects in a kcl file.
+///
+///     # get the mass of a file
+///     $ kittycad kcl mass --src_unit=m my-file.kcl
+///
+///     # pass a file from stdin
+///     $ cat my-file.kcl | kittycad kcl mass --src_unit=m
+#[derive(Parser, Debug, Clone)]
+#[clap(verbatim_doc_comment)]
+pub struct CmdKclMass {
+    /// The path to the input file.
+    /// If you pass `-` as the path, the file will be read from stdin.
+    #[clap(name = "input", required = true)]
+    pub input: std::path::PathBuf,
+
+    /// Material density.
+    #[clap(short = 'm', long = "material-density")]
+    material_density: f32,
+
+    /// Material density unit.
+    #[clap(long = "material-density-unit", value_enum)]
+    material_density_unit: kittycad::types::UnitDensity,
+
+    /// The source unit to use for the kcl file.
+    #[clap(long, short = 's', value_enum)]
+    pub src_unit: kittycad::types::UnitLength,
+
+    /// Output format.
+    #[clap(long, short, value_enum)]
+    pub format: Option<crate::types::FormatOutput>,
+
+    /// Output unit.
+    #[clap(long = "output-unit", short = 'u', value_enum)]
+    pub output_unit: kittycad::types::UnitMass,
+}
+
+#[async_trait::async_trait(?Send)]
+impl crate::cmd::Command for CmdKclMass {
+    async fn run(&self, ctx: &mut crate::context::Context) -> Result<()> {
+        if self.material_density == 0.0 {
+            anyhow::bail!("`--material-density` must not be 0.0");
+        }
+
+        // Get the contents of the input file.
+        let input = ctx.read_file(self.input.to_str().unwrap_or(""))?;
+        // Parse the input as a string.
+        let input = std::str::from_utf8(&input)?;
+
+        // Spin up websockets and do the conversion.
+        // This will not return until there are files.
+        let resp = ctx
+            .send_modeling_cmd(
+                "",
+                input,
+                kittycad::types::ModelingCmd::Mass {
+                    entity_ids: vec![], // get whole model
+                    material_density: self.material_density.into(),
+                    material_density_unit: self.material_density_unit.clone(),
+                    output_unit: self.output_unit.clone(),
+                    source_unit: self.src_unit.clone(),
+                },
+            )
+            .await?;
+
+        if let kittycad::types::OkWebSocketResponseData::Modeling {
+            modeling_response: kittycad::types::OkModelingCmdResponse::Mass { data },
+        } = &resp
+        {
+            // Print the output.
+            let format = ctx.format(&self.format)?;
+            ctx.io.write_output(&format, &data.mass)?;
+        } else {
+            anyhow::bail!("Unexpected response from engine: {:?}", resp);
+        }
+
+        Ok(())
+    }
+}
+
+/// Get the center of mass of objects in a kcl file.
+///
+///     # get the mass of a file
+///     $ kittycad kcl center-of-mass --src_unit=m my-file.kcl
+///
+///     # pass a file from stdin
+///     $ cat my-file.kcl | kittycad kcl center-of-mass --src_unit=m
+#[derive(Parser, Debug, Clone)]
+#[clap(verbatim_doc_comment)]
+pub struct CmdKclCenterOfMass {
+    /// The path to the input file.
+    /// If you pass `-` as the path, the file will be read from stdin.
+    #[clap(name = "input", required = true)]
+    pub input: std::path::PathBuf,
+
+    /// The source unit to use for the kcl file.
+    #[clap(long, short = 's', value_enum)]
+    pub src_unit: kittycad::types::UnitLength,
+
+    /// Output format.
+    #[clap(long, short, value_enum)]
+    pub format: Option<crate::types::FormatOutput>,
+
+    /// Output unit.
+    #[clap(long = "output-unit", short = 'u', value_enum)]
+    pub output_unit: kittycad::types::UnitLength,
+}
+
+#[async_trait::async_trait(?Send)]
+impl crate::cmd::Command for CmdKclCenterOfMass {
+    async fn run(&self, ctx: &mut crate::context::Context) -> Result<()> {
+        // Get the contents of the input file.
+        let input = ctx.read_file(self.input.to_str().unwrap_or(""))?;
+        // Parse the input as a string.
+        let input = std::str::from_utf8(&input)?;
+
+        // Spin up websockets and do the conversion.
+        // This will not return until there are files.
+        let resp = ctx
+            .send_modeling_cmd(
+                "",
+                input,
+                kittycad::types::ModelingCmd::CenterOfMass {
+                    entity_ids: vec![], // get whole model
+                    output_unit: self.output_unit.clone(),
+                    source_unit: self.src_unit.clone(),
+                },
+            )
+            .await?;
+
+        if let kittycad::types::OkWebSocketResponseData::Modeling {
+            modeling_response: kittycad::types::OkModelingCmdResponse::CenterOfMass { data },
+        } = &resp
+        {
+            // Print the output.
+            let format = ctx.format(&self.format)?;
+            ctx.io.write_output(&format, &data.center_of_mass)?;
+        } else {
+            anyhow::bail!("Unexpected response from engine: {:?}", resp);
+        }
+
+        Ok(())
+    }
+}
+
+/// Get the density of objects in a kcl file.
+///
+///     # get the density of a file
+///     $ kittycad kcl density --src_unit=m my-file.kcl
+///
+///     # pass a file from stdin
+///     $ cat my-file.kcl | kittycad kcl density --src_unit=m
+#[derive(Parser, Debug, Clone)]
+#[clap(verbatim_doc_comment)]
+pub struct CmdKclDensity {
+    /// The path to the input file.
+    /// If you pass `-` as the path, the file will be read from stdin.
+    #[clap(name = "input", required = true)]
+    pub input: std::path::PathBuf,
+
+    /// The source unit to use for the kcl file.
+    #[clap(long, short = 's', value_enum)]
+    pub src_unit: kittycad::types::UnitLength,
+
+    /// Material mass.
+    #[clap(short = 'm', long = "material-mass")]
+    material_mass: f32,
+
+    /// The unit of the material mass.
+    #[clap(long = "material-mass-unit", value_enum)]
+    material_mass_unit: kittycad::types::UnitMass,
+
+    /// Output format.
+    #[clap(long, short, value_enum)]
+    pub format: Option<crate::types::FormatOutput>,
+
+    /// Output unit.
+    #[clap(long = "output-unit", short = 'u', value_enum)]
+    pub output_unit: kittycad::types::UnitDensity,
+}
+
+#[async_trait::async_trait(?Send)]
+impl crate::cmd::Command for CmdKclDensity {
+    async fn run(&self, ctx: &mut crate::context::Context) -> Result<()> {
+        if self.material_mass == 0.0 {
+            anyhow::bail!("`--material-mass` must not be 0.0");
+        }
+
+        // Get the contents of the input file.
+        let input = ctx.read_file(self.input.to_str().unwrap_or(""))?;
+        // Parse the input as a string.
+        let input = std::str::from_utf8(&input)?;
+
+        // Spin up websockets and do the conversion.
+        // This will not return until there are files.
+        let resp = ctx
+            .send_modeling_cmd(
+                "",
+                input,
+                kittycad::types::ModelingCmd::Density {
+                    entity_ids: vec![], // get whole model
+                    material_mass: self.material_mass.into(),
+                    material_mass_unit: self.material_mass_unit.clone(),
+                    output_unit: self.output_unit.clone(),
+                    source_unit: self.src_unit.clone(),
+                },
+            )
+            .await?;
+
+        if let kittycad::types::OkWebSocketResponseData::Modeling {
+            modeling_response: kittycad::types::OkModelingCmdResponse::Density { data },
+        } = &resp
+        {
+            // Print the output.
+            let format = ctx.format(&self.format)?;
+            ctx.io.write_output(&format, &data.density)?;
+        } else {
+            anyhow::bail!("Unexpected response from engine: {:?}", resp);
+        }
+
+        Ok(())
+    }
+}
+
+/// Get the surface area of objects in a kcl file.
+///
+///     # get the surface-area of a file
+///     $ kittycad kcl surface-area --src_unit=m my-file.kcl
+///
+///     # pass a file from stdin
+///     $ cat my-file.kcl | kittycad kcl surface-area --src_unit=m
+#[derive(Parser, Debug, Clone)]
+#[clap(verbatim_doc_comment)]
+pub struct CmdKclSurfaceArea {
+    /// The path to the input file.
+    /// If you pass `-` as the path, the file will be read from stdin.
+    #[clap(name = "input", required = true)]
+    pub input: std::path::PathBuf,
+
+    /// The source unit to use for the kcl file.
+    #[clap(long, short = 's', value_enum)]
+    pub src_unit: kittycad::types::UnitLength,
+
+    /// Output format.
+    #[clap(long, short, value_enum)]
+    pub format: Option<crate::types::FormatOutput>,
+
+    /// Output unit.
+    #[clap(long = "output-unit", short = 'u', value_enum)]
+    pub output_unit: kittycad::types::UnitArea,
+}
+
+#[async_trait::async_trait(?Send)]
+impl crate::cmd::Command for CmdKclSurfaceArea {
+    async fn run(&self, ctx: &mut crate::context::Context) -> Result<()> {
+        // Get the contents of the input file.
+        let input = ctx.read_file(self.input.to_str().unwrap_or(""))?;
+        // Parse the input as a string.
+        let input = std::str::from_utf8(&input)?;
+
+        // Spin up websockets and do the conversion.
+        // This will not return until there are files.
+        let resp = ctx
+            .send_modeling_cmd(
+                "",
+                input,
+                kittycad::types::ModelingCmd::SurfaceArea {
+                    entity_ids: vec![], // get whole model
+                    output_unit: self.output_unit.clone(),
+                    source_unit: self.src_unit.clone(),
+                },
+            )
+            .await?;
+
+        if let kittycad::types::OkWebSocketResponseData::Modeling {
+            modeling_response: kittycad::types::OkModelingCmdResponse::SurfaceArea { data },
+        } = &resp
+        {
+            // Print the output.
+            let format = ctx.format(&self.format)?;
+            ctx.io.write_output(&format, &data.surface_area)?;
+        } else {
+            anyhow::bail!("Unexpected response from engine: {:?}", resp);
+        }
+
+        Ok(())
+    }
+}
+
+/// Get the extension for a path buffer.
+pub fn get_extension(path: std::path::PathBuf) -> String {
+    path.into_boxed_path()
+        .extension()
+        .unwrap_or_default()
+        .to_str()
+        .unwrap_or("")
+        .to_string()
 }
