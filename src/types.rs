689329b0369edaf8d2bb889f89c5cb722e396531
49lf <ircsurfer33@gmail.com>
2024-03-12 13:30:19 -0400

    Add support for snapshotting gltf standard

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,5 @@
 use parse_display::{Display, FromStr};
+use serde::Deserialize;
 
 #[derive(Debug, Clone, PartialEq, Eq, FromStr, Display, clap::ValueEnum)]
 #[display(style = "kebab-case")]
@@ -14,4 +15,14 @@ impl FormatOutput {
     pub fn variants() -> Vec<String> {
         vec!["table".to_string(), "json".to_string(), "yaml".to_string()]
     }
+}
+
+#[derive(Deserialize)]
+pub struct GltfStandardBuffer {
+    pub uri: String,
+}
+
+#[derive(Deserialize)]
+pub struct GltfStandardJsonLite {
+    pub buffers: Vec<GltfStandardBuffer>,
 }
