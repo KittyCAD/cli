e850280a13f0aad2d4d89efa36acf286b55f7166
Jonathan Tran <jonnytran@gmail.com>
2025-02-18 15:01:21 -0500

    Update expected mass

--- src/tests.rs
+++ src/tests.rs
@@ -487,7 +487,7 @@ access-control-allow-credentials:  """#
                 "--material-density-unit".to_string(),
                 "lb-ft3".to_string(),
             ],
-            want_out: r#"68.4585"#.to_string(),
+            want_out: r#"68.458"#.to_string(),
             want_err: "".to_string(),
             want_code: 0,
             ..Default::default()
