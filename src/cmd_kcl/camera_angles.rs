fb9bdb03b3bd71c6dbbcecdece3d514a8822df34
Jonathan Tran <jonnytran@gmail.com>
2026-01-15 09:51:48 -0500

    Release 142 (#1421)
    * Release 142
    
    * Change to use the new builder API

--- src/cmd_kcl/camera_angles.rs
+++ src/cmd_kcl/camera_angles.rs
@@ -1,28 +1,42 @@
 use kittycad_modeling_cmds as kcmc;
 
-pub(crate) const FRONT: kcmc::ModelingCmd = kcmc::ModelingCmd::DefaultCameraLookAt(kcmc::DefaultCameraLookAt {
-    up: kcmc::shared::Point3d { x: 0.0, y: 0.0, z: 1.0 },
-    vantage: kcmc::shared::Point3d {
-        x: 0.0,
-        y: -1.0,
-        z: 0.0,
-    },
-    center: kcmc::shared::Point3d { x: 0.0, y: 0.0, z: 0.0 },
-    sequence: None,
-});
+pub(crate) fn front() -> kcmc::ModelingCmd {
+    kcmc::ModelingCmd::DefaultCameraLookAt(
+        kcmc::DefaultCameraLookAt::builder()
+            .up(kcmc::shared::Point3d { x: 0.0, y: 0.0, z: 1.0 })
+            .vantage(kcmc::shared::Point3d {
+                x: 0.0,
+                y: -1.0,
+                z: 0.0,
+            })
+            .center(kcmc::shared::Point3d { x: 0.0, y: 0.0, z: 0.0 })
+            .maybe_sequence(None)
+            .build(),
+    )
+}
 
-pub(crate) const RIGHT_SIDE: kcmc::ModelingCmd = kcmc::ModelingCmd::DefaultCameraLookAt(kcmc::DefaultCameraLookAt {
-    up: kcmc::shared::Point3d { x: 0.0, y: 0.0, z: 1.0 },
-    vantage: kcmc::shared::Point3d { x: 1.0, y: 0.0, z: 0.0 },
-    center: kcmc::shared::Point3d { x: 0.0, y: 0.0, z: 0.0 },
-    sequence: None,
-});
+pub(crate) fn right_side() -> kcmc::ModelingCmd {
+    kcmc::ModelingCmd::DefaultCameraLookAt(
+        kcmc::DefaultCameraLookAt::builder()
+            .up(kcmc::shared::Point3d { x: 0.0, y: 0.0, z: 1.0 })
+            .vantage(kcmc::shared::Point3d { x: 1.0, y: 0.0, z: 0.0 })
+            .center(kcmc::shared::Point3d { x: 0.0, y: 0.0, z: 0.0 })
+            .maybe_sequence(None)
+            .build(),
+    )
+}
 
-pub(crate) const TOP: kcmc::ModelingCmd = kcmc::ModelingCmd::DefaultCameraLookAt(kcmc::DefaultCameraLookAt {
-    up: kcmc::shared::Point3d { x: 0.0, y: 1.0, z: 0.0 },
-    vantage: kcmc::shared::Point3d { x: 0.0, y: 0.0, z: 1.0 },
-    center: kcmc::shared::Point3d { x: 0.0, y: 0.0, z: 0.0 },
-    sequence: None,
-});
+pub(crate) fn top() -> kcmc::ModelingCmd {
+    kcmc::ModelingCmd::DefaultCameraLookAt(
+        kcmc::DefaultCameraLookAt::builder()
+            .up(kcmc::shared::Point3d { x: 0.0, y: 1.0, z: 0.0 })
+            .vantage(kcmc::shared::Point3d { x: 0.0, y: 0.0, z: 1.0 })
+            .center(kcmc::shared::Point3d { x: 0.0, y: 0.0, z: 0.0 })
+            .maybe_sequence(None)
+            .build(),
+    )
+}
 
-pub(crate) const ISO: kcmc::ModelingCmd = kcmc::ModelingCmd::ViewIsometric(kcmc::ViewIsometric { padding: 0.0 });
+pub(crate) fn iso() -> kcmc::ModelingCmd {
+    kcmc::ModelingCmd::ViewIsometric(kcmc::ViewIsometric::builder().padding(0.0).build())
+}
