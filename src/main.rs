3731c8c57d39d1b44f0d81dc7392b7b66828840b
Jonathan Tran <jonnytran@gmail.com>
2026-05-21 13:47:36 -0400

    Release 172 and bump kcl (#1645)
    * Release 172 and bump kcl
    
    * Fix build error

--- src/main.rs
+++ src/main.rs
@@ -319,8 +319,7 @@ async fn run_cmd(cmd: &impl crate::cmd::Command, ctx: &mut context::Context<'_>)
                     writeln!(ctx.io.err_out, "{} You are not authenticated.", cs.failure_icon())?;
 
                     writeln!(ctx.io.err_out, "Try authenticating with: `zoo auth login`")?;
-                } else if let kittycad::types::error::Error::UnexpectedResponse(resp) = err {
-                    let body = resp.text().await?;
+                } else if let kittycad::types::error::Error::UnexpectedResponse { body, .. } = err {
                     writeln!(ctx.io.err_out, "zoo.dev api error: {body}")?;
                 } else {
                     writeln!(ctx.io.err_out, "{err}")?;
